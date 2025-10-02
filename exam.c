d16816a6735f4c3eddc135eaa8dfdde72c6b348d
PA106/G/28720/25 <chepsiddy@gmail.com>
2025-10-02 13:40:06 +0300

    Add files via upload

---
+++ exam.c
@@ -0,0 +1,26 @@
+/* 
+
+Name:Siddy Chepkurui
+Reg no:PA106/G/28720/25
+Description:program to check exam eligibility
+
+*/
+ #include <stdio.h>
+
+int main(){
+float attendance,average_marks
+
+printf("enter attendance:");
+scanf("%f",&attendance );
+
+print("enter average marks:");
+scanf("%f",&average_marks);
+
+if(attendance>=75% &&average_marks>=40)
+print("Eligible\n");
+else:
+print("Not eligible\n");        
+
+
+return 0;
+}

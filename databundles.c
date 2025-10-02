afe78b0cebc5e5825859430870a85f82674be868
PA106/G/28720/25 <chepsiddy@gmail.com>
2025-10-02 14:52:38 +0300

    Add files via upload

---
+++ databundles.c
@@ -0,0 +1,19 @@
+//
+Name:Siddy Chepkurui
+Reg no:PA106/G/28720/25
+Program:display data bundles 
+
+
+//program to display data bundles
+include <stdio.h>
+
+int main(){  
+    print("Mobile Data Bundles")
+    print("Option  Bundles Cost(kES)")
+    print("1       100 MB   50")
+    print("2       500 MB   200")
+    print("3       1 GB     350")
+    print("4       2 GB     600") 
+
+    return 0;
+}

cf3a686293133cf949fe67d706b80407e8ae8b17
PA106/G/28720/25 <chepsiddy@gmail.com>
2025-10-09 05:57:57 +0300

    Add files via upload

---
+++ password.c
@@ -0,0 +1,26 @@
+
+Name: Siddy Chepkurui
+Reg no:PA106/G/28720/25
+
+
+/*
+program to prompt the user to enter a password until the correct password is entered.
+*/
+
+#include <stdio h.>
+
+int main(){
+   char password[20]
+int i=0; // start
+     do {
+    int password
+    int correct password = "1234"://start
+    printf("Enter password")
+    scanf("%s",password)k;
+     
+     }while(strcmp(password,1234)!=0);
+
+    printf("Access Granted\n")
+
+    return 0;
+     }

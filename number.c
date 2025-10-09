45ff351873c48ab6b190c070d4500fba6c1a87ac
PA106/G/28720/25 <chepsiddy@gmail.com>
2025-10-09 05:57:27 +0300

    Add files via upload

---
+++ number.c
@@ -0,0 +1,16 @@
+
+Name:Siddy Chepkurui
+Reg no:PA106/G/28720/25
+
+
+//write a program to print numbers from 100 to 50 using for loop
+
+
+#include<stdio.h>
+
+int main(){
+    int i;
+    for(i=100; i >=50)
+        printf("%d\n",i--);
+        return 0;
+}
